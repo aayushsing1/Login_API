1b3eb13dd74999e57aae2b7c634efbd92d29aaef
Aayush kr. Singh <111671884+aayushsing1@users.noreply.github.com>
2024-05-02 19:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 # MongoDB configuration
 uri = 'mongodb+srv://asdevlopers02:6fXfDFKNImSUAiKJ@cluster0.us8nw6q.mongodb.net/'
